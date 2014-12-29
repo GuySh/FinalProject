b6e4c5b4aded9417720afd1f5ee15c8ce8bf90da
GuySh <rakzevel1@gmail.com>
2014-12-29 11:52:53 +0200

    initialize commit

---
+++ Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSprite : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public string resourceName;
+	public int currentsprite = -1;
+
+	// Use this for initialization
+	void Start () {
+		if (resourceName != "") 
+		{
+			sprites = Resources.LoadAll<Sprite>(resourceName);
+
+			if(currentsprite == -1)
+			{
+				currentsprite = Random.Range(0, sprites.Length);
+			}
+			else if(currentsprite > sprites.Length)
+			{
+				currentsprite = sprites.Length-1;
+			}
+
+			GetComponent<SpriteRenderer>().sprite = sprites[currentsprite]; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TouchCode.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchCode : MonoBehaviour {
+
+
+	public Vector2 moving = new Vector2();
+	public string direction = "";
+
+	Player player;
+	public GameObject respawnPrefab;
+	public GameObject respawn;
+
+	// Use this for initialization
+	void Start () {
+		//player = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		//player.moving.y = 0;
+		//player.GetComponentInChildren (PlayerController);
+		moving.x = moving.y = 0;
+		//player.moving.x = player.moving.y = 0;
+
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+			if(direction == "up")
+			{
+				player.moving.y = 1;
+				moving.y = 1;
+				//player.setUp();
+			}
+			
+		}
+		if (fingerCount > 0)
+			print("User has " + fingerCount + " finger(s) touching the screen");
+		
+	
+	}
+}
